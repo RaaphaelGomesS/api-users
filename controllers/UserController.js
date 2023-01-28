56bbec91ef59f43dbc02e76db0e298d53f12e2c0
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2023-01-28 08:50:52 -0300

    feat: req de cadastro de usuários

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,28 @@
-class UserController{
+var User = require("../services/User.js");
 
-    async index(req, res){
+class UserController {
+  async index(req, res) {}
 
+  async create(req, res) {
+    const { email, name, password } = req.body;
+
+    if (email == undefined) {
+      res.status(400);
+      res.json({ err: "o e-mail é inválido!" });
+      return;
     }
 
-    async create(req, res){
-        res.send("catch body req!")
+    const emailExist = await User.findEmail(email);
+
+    if (emailExist) {
+      res.status(406);
+      res.json({ err: "o email já está cadastrado!" });
+    } else if (emailExist == false) {
+      await User.new(email, name, password);
+      res.status(200);
+      res.json({ msg: "Usuário criado!" });
     }
-
-
+  }
 }
 
 module.exports = new UserController();
