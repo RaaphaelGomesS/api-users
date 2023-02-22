b359063ff4b135830898c65fba0cff2910fed197
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2023-02-22 16:24:40 -0300

    feat: cria os metodos para mnipulação do token

---
+++ services/PasswordToken.js
@@ -0,0 +1,60 @@
+var knex = require("../database/connection.js");
+const User = require("./User.js");
+
+class PasswordToken {
+  async create(email) {
+    const user = await User.findByEmail(email);
+
+    if (user != undefined) {
+      try {
+        var token = Date.now();
+
+        await knex
+          .insert({
+            user_id: user.id,
+            used: 0,
+            token: token,
+          })
+          .table("passwordtokens");
+
+        return { status: true, token: token };
+      } catch (error) {
+        console.log(error);
+        return { status: false, err: err };
+      }
+    } else {
+      return { status: false, err: "O e-mail não existe no banco!" };
+    }
+  }
+
+  async validate(token) {
+    try {
+      var result = await knex
+        .select()
+        .where({ token: token })
+        .table("passwordtokens");
+
+      if (result.length > 0) {
+        var tk = result[0];
+        if (tk.used) {
+          return { status: false };
+        } else {
+          return { status: true, token: tk };
+        }
+      } else {
+        return { status: false };
+      }
+    } catch (error) {
+      return { status: false };
+    }
+  }
+
+  async setUsed(token) {
+    await knex
+      .update({ used: 1 })
+      .where({ token: token })
+      .table("passwordtokens");
+  }
+}
+
+module.exports = new PasswordToken();
