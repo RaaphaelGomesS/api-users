561c58f2f92e445c7a03d1025e9c6281449aac2a
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2023-02-23 14:02:51 -0300

    fix: remove codigo teste

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,9 @@
 const express = require("express")
 const app = express();
 const router = express.Router();
-const HomeController = require("../controllers/HomeController");
 const UserController = require("../controllers/UserController");
 const AdminAuth = require("../middleware/AdminAuth");
 
-router.get('/', HomeController.index);
 router.post('/user', UserController.create);
 router.get('/users', AdminAuth, UserController.index);
 router.get("/user/:id", AdminAuth, UserController.findUser);
